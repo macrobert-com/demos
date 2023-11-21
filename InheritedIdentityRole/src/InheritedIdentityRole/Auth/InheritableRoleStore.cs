b1031b46eb916c3bff354188fa562cd4e1f36734
machonky <dev@macrobert.com>
2023-11-21 22:19:37 +0800

    CTE Implementtation with LinqToDb

--- InheritedIdentityRole/src/InheritedIdentityRole/Auth/InheritableRoleStore.cs
+++ InheritedIdentityRole/src/InheritedIdentityRole/Auth/InheritableRoleStore.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LinqToDB;
+using LinqToDB.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-using Dapper;
 
 namespace InheritedIdentityRole.Auth;
 
@@ -16,55 +17,29 @@ public class InheritableRoleStore<TContext>
     private DbSet<RoleHierarchy> RoleHierarchies { get { return Context.Set<RoleHierarchy>(); } }
     private DbSet<IdentityRoleClaim<string>> RoleClaims { get { return Context.Set<IdentityRoleClaim<string>>(); } }
 
+    static InheritableRoleStore()
+    {
+        LinqToDBForEFTools.Initialize();
+    }
+
     public async Task<List<InheritableIdentityRole>> GetInheritedRolesAsync(InheritableIdentityRole role, CancellationToken cancellationToken = default(CancellationToken))
     {
-        const string InheritedRolesQuery = @"
-WITH InheritedRoles AS (
-    SELECT r.Id, r.Name
-    FROM AspNetRoles r
-    WHERE r.Id = @RoleId
-    UNION ALL
-    SELECT r.Id, r.Name
-    FROM AspNetRoles r
-    JOIN RoleHierarchies ir ON r.Id = ir.ParentRoleId
-    JOIN InheritedRoles ON ir.ChildRoleId = InheritedRoles.Id
-)
-SELECT InheritedRoles.Id, InheritedRoles.Name FROM InheritedRoles;
-";
-        using var connection = Context.Database.GetDbConnection();
-        var result = await connection.QueryAsync<InheritableIdentityRole>(InheritedRolesQuery, new { RoleId = role.Id });
-        var inheritedRoles = new List<InheritableIdentityRole>(result);
+        using var dc = Context.CreateLinqToDBConnection();
+
+        var recursiveRoleCte = dc.GetCte<InheritableIdentityRole>("InheritedRoles", query =>        
+           (from r in Roles.Where(r => r.Id == role.Id) select r)
+            .Concat(
+                from rh in RoleHierarchies
+                join cq in query on rh.ChildRoleId equals cq.Id
+                select new InheritableIdentityRole { Id = rh.ParentRoleId, Name = cq.Name })
+        );
+
+        var cteQuery =
+            from cteRow in recursiveRoleCte
+            join r in Roles on cteRow.Id equals r.Id
+            select r;
+
+        var inheritedRoles = await cteQuery.ToListAsyncLinqToDB(cancellationToken);
         return inheritedRoles;
-    }
-
-    private async Task<List<InheritableIdentityRole>> GetChildRolesAsync(InheritableIdentityRole role, CancellationToken cancellationToken)
-    {
-        return await RoleHierarchies
-            .Where(rh => rh.ChildRoleId == role.Id)
-            .Select(rh => rh.ParentRole)
-            .ToListAsync(cancellationToken);
-    }
-
-    public override async Task<IList<Claim>> GetClaimsAsync(InheritableIdentityRole role, CancellationToken cancellationToken = default(CancellationToken))
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        ThrowIfDisposed();
-        ArgumentNullException.ThrowIfNull(role);
-
-        var allRoles = await GetInheritedRolesAsync(role);
-        allRoles.Add(role);
-
-        var allClaims = new List<Claim>();
-        allRoles.ForEach(async r =>
-        {
-            var roleClaims = await RoleClaims
-                .Where(rc => rc.RoleId.Equals(r.Id))
-                .Select(c => new Claim(c.ClaimType!, c.ClaimValue!))
-                .ToListAsync(cancellationToken);
-
-            allClaims.AddRange(roleClaims);
-        });
-
-        return allClaims;
     }
 }
