bae8c5b991de196c3a18b8681cb452e26ffc03a2
machonky <dev@macrobert.com>
2023-11-22 11:19:40 +0800

    Logging added to show resulting SQL in Log output

--- InheritedIdentityRole/src/InheritedIdentityRole/Auth/InheritableRoleStore.cs
+++ InheritedIdentityRole/src/InheritedIdentityRole/Auth/InheritableRoleStore.cs
@@ -1,7 +1,9 @@
 ﻿using LinqToDB;
+using LinqToDB.Data;
 using LinqToDB.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Security.Claims;
 
 namespace InheritedIdentityRole.Auth;
@@ -11,20 +13,30 @@ public class InheritableRoleStore<TContext>
     , IInheritableRoleStore
     where TContext : DbContext
 {
-    public InheritableRoleStore(TContext context, IdentityErrorDescriber? describer = null) : base(context, describer)
-    {}
+    public InheritableRoleStore(ILogger<InheritableRoleStore<TContext>> logger, TContext context, IdentityErrorDescriber? describer = null) 
+        : base(context, describer)
+    {
+        this.Logger = logger;
+        DataConnection.WriteTraceLine = (message, category, level) => Logger.LogInformation(message);
+    }
 
     private DbSet<RoleHierarchy> RoleHierarchies { get { return Context.Set<RoleHierarchy>(); } }
     private DbSet<IdentityRoleClaim<string>> RoleClaims { get { return Context.Set<IdentityRoleClaim<string>>(); } }
 
+    public ILogger<InheritableRoleStore<TContext>> Logger { get; }
+
     static InheritableRoleStore()
     {
         LinqToDBForEFTools.Initialize();
+#if DEBUG
+        DataConnection.TurnTraceSwitchOn();
+#endif
     }
 
     public async Task<List<InheritableIdentityRole>> GetInheritedRolesAsync(InheritableIdentityRole role, CancellationToken cancellationToken = default(CancellationToken))
     {
         using var dc = Context.CreateLinqToDBConnection();
+        //Logger.LogInformation("LinqToDBConnection Created");
 
         var recursiveRoleCte = dc.GetCte<InheritableIdentityRole>("InheritedRoles", query =>        
            (from r in Roles.Where(r => r.Id == role.Id) select r)
@@ -33,13 +45,18 @@ public class InheritableRoleStore<TContext>
                 join cq in query on rh.ChildRoleId equals cq.Id
                 select new InheritableIdentityRole { Id = rh.ParentRoleId, Name = cq.Name })
         );
+        //Logger.LogInformation("Recursive CTE defined");
 
         var cteQuery =
             from cteRow in recursiveRoleCte
             join r in Roles on cteRow.Id equals r.Id
             select r;
 
+        //Logger.LogInformation("Final Role Query defined");
+
         var inheritedRoles = await cteQuery.ToListAsyncLinqToDB(cancellationToken);
+        //Logger.LogInformation("Query executed and results materialized");
+
         return inheritedRoles;
     }
 }
